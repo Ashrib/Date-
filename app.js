0e770f6264518ce1ea9b3f781d87868f5aef3839
Asharib Ali <96954816+Ashrib@users.noreply.github.com>
2022-06-21 21:39:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// Q.1....
+// var pIntegers = Number(prompt("Enter any positive integer"));
+// var pIntRound = Math.round(pIntegers);
+// var pIntFloor = Math.floor(pIntegers);
+// var pIntCeil = Math.ceil(pIntegers);
+
+// document.getElementById("numbr").innerHTML = "<b>Number: </b>"+ pIntegers;
+// document.getElementById("round").innerHTML = "<b>Round: </b>"+ pIntRound;
+// document.getElementById("floor").innerHTML = "<b>Floor: </b>"+ pIntFloor;
+// document.getElementById("ceil").innerHTML = "<b>Ceil: </b>"+ pIntCeil;
+// Q.2.....
+var nIntegers = Number(prompt("Enter any negative integer"));
+var nIntRound = Math.round(nIntegers);
+var nIntFloor = Math.floor(nIntegers);
+var nIntCeil = Math.ceil(nIntegers);
+
+document.getElementById("numbr").innerHTML = "<b>Number: </b>"+ nIntegers;
+document.getElementById("round").innerHTML = "<b>Round: </b>"+ nIntRound;
+document.getElementById("floor").innerHTML = "<b>Floor: </b>"+ nIntFloor;
+document.getElementById("ceil").innerHTML = "<b>Ceil: </b>"+ nIntCeil;
